0d204ba6d8783a7a794f8e9bb74a084cbe60a5b2
a-khoirunnufus <a.khoirunnufus@gmail.com>
2021-05-08 11:45:50 +0700

    admin dashboard progress view

--- app/Views/admin/admin_layout.php
+++ app/Views/admin/admin_layout.php
@@ -9,90 +9,44 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">    
     <!-- bootstrap icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
     <!-- custom css for dashboard -->
-    <link rel="stylesheet" href="<?= base_url(); ?>/public/css/dashboard.css" />
-    <!-- Custom CSS -->
-    <!-- <link rel="stylesheet" href="<?= base_url(); ?>/public/css/style.css" /> -->
+    <link rel="stylesheet" href="<?= base_url(); ?>/public/css/admin/dashboard.css" />
 </head>
 <body>
-  <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">GERATISIN</a>
-    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
-    <ul class="navbar-nav px-3">
-      <li class="nav-item text-nowrap">
-        <a class="nav-link" href="#">Sign out</a>
-      </li>
-    </ul>
-  </header>
-
-  <div class="container-fluid">
-    <div class="row">
-      <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-        <div class="position-sticky pt-3">
-          <ul class="nav flex-column">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
-                Dashboard
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <i class="bi bi-people"></i>
-                Pengguna Publik
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
-        <p>Selamat datang, <?= $user_fullname ?></p>
-      </main>
-    </div>
-  </div>
-
-  <!-- old materialize sidenav -->
-	<!-- <ul class="sidenav sidenav-fixed">
-    <li>
-    	<div class="user-view">
-        <div class="background">
-          <img src="<?= base_url(); ?>/public/img/background03.jpg">
-        </div>
-        <a href="#user"><img class="circle user-avatar" src="<?= base_url(); ?>/public/img/person01.jpg"></a>
-        <a href="#name"><span class="white-text name"><?= $user_fullname ?></span></a>
+  <div class="wrapper">
+    <!-- SIDEBAR START -->
+    <div class="sidebar bg-dark text-light">
+      <div class="brand text-center">
+        <h5 class="brand-text">GERATISIN</h5>
       </div>
-    </li>
-    <li><a class="waves-effect" href="#!"><i class="material-icons">people</i>Pengguna Publik</a></li>
-  </ul>
-  <div class="main-wrapper">
-    <div class="header">
-      <h5>Manage Pengguna Publik</h5>
+      <div class="h-100">
+        <ul class="sidebar-nav">
+          <li>
+            <a class="sidebar-nav-link" href="#">
+              <i class="bi bi-speedometer"></i>
+              <span>Dashboard</span>
+            </a>
+          </li>
+          <li>
+            <a class="sidebar-nav-link" href="#">
+              <i class="bi bi-people"></i>
+              <span>Pengguna Publik</span>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="main-content">
-    
+    <!-- SIDEBAR END -->
+
+    <main>
+      <!-- HEADER START -->
+      <header>
+        header ...
+      </header>
+      <!-- HEADER END -->
       <?= $this->renderSection('main') ?>
-    
-    </div>
-  </div> -->
+    </main>
+  </div>
 
   <!-- Bootstrap JavaScript -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
